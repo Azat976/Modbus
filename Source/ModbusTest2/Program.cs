5c3a45d0d7dcbe09dc7c698db9a5b84b1f790e9c
Исламов Азат Ильхамович <Islamov-AI@nft.local>
2020-09-25 15:14:45 +0500

    First version modbus

---
+++ Source/ModbusTest2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using ModbusTest2;
+
+namespace TCPModbus
+{
+
+    #region   
+    class Header
+    {
+
+        ////Для заголовка: id-идентефикатор(в дальнейшем он конвертируется в байты(можно было и его через байты), IdProt-Идентификатор протокола, lenghtMod-длинна сообщения протокола модбас
+        //static ushort id = 1;
+        //static byte[] IdProt = { 0, 0 };
+        //static byte lenghtMod = 0;
+
+        ////Для формирования функции: len- длинна сообщения, func-функция
+        //static byte[] len = { 6, 11, 8 };
+        //static byte unit = 1;
+        //static byte[] func = { 1, 2, 3, 4, 5, 6, 15, 16 };
+
+        ////Для стартого адреса, везде стоит 0.1, в некоторых длинна сообщения, и кол во байт выставлены сразу как константы!(для примера)
+        //static byte[] startAddress = { 0, 1 };
+
+
+
+        public byte[] func1 = {0, 1, 0, 0, 0};
+        public byte[] Header1(ushort id, byte[] IdProt, byte lenghtMod)
+        {
+            byte[] data = new byte[5];
+            byte[] _id = BitConverter.GetBytes((short)id);
+            data[0] = _id[1];
+            data[1] = _id[0];
+            data[2] = IdProt[0];
+            data[3] = IdProt[1];
+            data[4] = lenghtMod;
+            return data;
+        }
+    }
+
+    class Podkl
+    {
+        public const int port = 506;
+        public const string server = "127.0.0.1";
+    }
+    #endregion
+    public class Program
+    {
+
+        //Для заголовка: id-идентефикатор(в дальнейшем он конвертируется в байты(можно было и его через байты), IdProt-Идентификатор протокола, lenghtMod-длинна сообщения протокола модбас
+        static ushort id = 1;
+        static byte[] IdProt = { 0, 0 };
+        static byte lenghtMod = 0;
+
+        //Для формирования функции: len- длинна сообщения, func-функция
+        static byte[] len = { 6, 11, 8 };
+        static byte unit = 1;
+        static byte[] func = { 1, 2, 3, 4, 5, 6, 15, 16 };
+
+        //Для стартого адреса, везде стоит 0.1, в некоторых длинна сообщения, и кол во байт выставлены сразу как константы!(для примера)
+        static byte[] startAddress = { 0, 1 };
+
+
+
+        #region 
+        //Можно было и так...
+        //byte[] _id = BitConverter.GetBytes((short)id);
+        //static byte[] PocketByte1 = { _id[1], _id[0], 0, 0, 0, 6, 1, 1, 0, 1, 3, 4 };
+        //static byte[] PocketByte2 = { 0, 1, 0, 0, 0, 6, 1, 2, 0, 1, 0, 2 };
+        //static byte[] PocketByte3 = { 0, 1, 0, 0, 0, 6, 1, 3, 0, 1, 0, 6 };
+        //static byte[] PocketByte4 = { 0, 1, 0, 0, 0, 6, 1, 4, 0, 1, 0, 6 };
+        //static byte[] PocketByte5 = { 0, 1, 0, 0, 0, 6, 1, 5, 0, 1, 0, 1 };
+        //static byte[] PocketByte6 = { 0, 1, 0, 0, 0, 6, 1, 6, 0, 1, 1, 2 };
+        //static byte[] PocketByte7 = { 0, 1, 0, 0, 0, 8, 1, 15, 0, 3, 0, 5, 1, 19 };
+        //static byte[] PocketByte8 = { 0, 1, 0, 0, 0, 11, 1, 16, 0, 3, 0, 2, 4, 0, 10, 1, 2 };
+        #endregion
+
+//        public ModbusTest2.Master MB;
+//        static ModbusTest2.Podkl P;
+
+        public const int port = 507;
+        public const string server = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            ModbusTest2.Master.Connect(server, port);
+            Console.Write(" Выберите пункт меню: ");
+            String strr = Console.ReadLine();
+
+
+            switch (strr)
+            {
+                //
+                case "1":
+                    ModbusTest2.Master.ReadCoilStatus(len, unit, func, startAddress);
+
+                    break;
+                //
+                case "2":
+                    ModbusTest2.Master.ReadInputStatus(len, unit, func, startAddress);
+                    break;
+
+                case "3":
+                    ModbusTest2.Master.ReadHoldingReg(len, unit, func, startAddress);
+                    break;
+
+                case "4":
+                    ModbusTest2.Master.ReadInputReg(len, unit, func, startAddress);
+                    break;
+                //    //
+                case "5":
+                    ModbusTest2.Master.ForceSingleCoil(len, unit, func, startAddress);
+                    break;
+                //    //
+                case "6":
+                    ModbusTest2.Master.PresetSingleReg(len, unit, func, startAddress);
+                    break;
+                //    //
+                case "7":
+                    ModbusTest2.Master.ForceMultipleCoil(len, unit, func, startAddress);
+                    break;
+                //    //
+                case "8":
+                    ModbusTest2.Master.PresetMultipleReg(len, unit, func, startAddress);
+                    break;
+                    //}
+            }
+
+
+        }
+    }
+}
+
---
+++ Source/ModbusTest2/Reg.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ModbusTest2
+{
+
+    public class Reg
+    {
+        public static byte[] func1 = { 0, 1, 0, 0, 0 };
+    }
+
+    public class Master
+    {
+        public string inIp1 = "127.0.0.1";
+        public int port1 = 507;
+
+        List<byte> Header;
+
+             //public Master(string inIp, int port, string slaveid)
+        //{
+        //    inIp1 = inIp;
+        //    port1 = port;
+        //}
+        private static TcpClient TCPMBus= new TcpClient();
+        //private static byte[] TCPMBusBuffer = new byte[2048];
+
+        #region
+        public static void Connect(string server, int port)
+        {
+            //TcpClient TCPMBus = new TcpClient();
+            TCPMBus.ReceiveBufferSize = 48;
+            TCPMBus.Connect(server, port);
+            NetworkStream tcpStream = TCPMBus.GetStream();
+
+
+        }
+        #endregion
+
+    
+
+
+        public static void WriteData(/*string server, int port,*/byte[] data)
+        {
+            //TcpClient TCPMBus = new TcpClient();
+            //TCPMBus.ReceiveBufferSize = 48;
+            //TCPMBus.Connect(server, port);
+            NetworkStream tcpStream = TCPMBus.GetStream();
+            tcpStream.Write(data, 0, data.Length);
+            
+            byte[] bytes = new byte[TCPMBus.ReceiveBufferSize];
+            int bytesRead = tcpStream.Read(bytes, 0, TCPMBus.ReceiveBufferSize);
+            byte[] bytes1 = new byte[TCPMBus.ReceiveBufferSize];
+            int bytesRead1 = tcpStream.Read(bytes1, 0, TCPMBus.ReceiveBufferSize);
+            TCPMBus.Close();
+            // Строка, содержащая ответ от сервера
+            string returnData = BitConverter.ToString(bytes);
+            Console.WriteLine("Ответ: " + returnData);
+            byte[] values = new byte[2];
+            int str = Convert.ToInt32(data[7]);
+            for (int j = 0; j < 8; j++)
+            {
+                values[1] = (byte)(bytes[9 + (j * 2)]);
+                values[0] = (byte)(bytes[10 + (j * 2)]);
+                if (str == 3)
+                    Console.WriteLine("распознанные данные, регистр " + (j + 1) + ": " + BitConverter.ToUInt16(values, 0));
+            }
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+        public static byte[] FullPocket(byte[] data)
+        {
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            WriteData(mess1);
+            return mess1;
+        }
+
+
+        public static void ReadCoilStatus(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[0];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+                //Количество регистров, далее сделано всё так же
+            data[5] = 0;
+            data[6] = 2;
+            //byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(data);
+            Console.WriteLine("Запрос Read Coil Status:  00-01-00-00-00-" + msd1);
+            FullPocket(data);
+        
+
+
+            //byte[] data = new byte[7];
+            //data[0] = len[0];
+            //data[1] = unit;
+            //data[2] = func[0];
+            //data[3] = startAddress[0];
+            //data[4] = startAddress[1];
+            ////Количество регистров, далее сделано всё так же
+            //data[5] = 0;
+            //data[6] = 2;
+            ////byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            //string msd1 = BitConverter.ToString(data);
+            //Console.WriteLine("Запрос Read Coil Status:  00-01-00-00-00-" + msd1);
+            //FullPocket(data);
+            //return data;
+            ////return data;
+
+        }
+        public static void ReadInputStatus(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[1];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+            data[5] = 0;
+            data[6] = 2;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Read Input Status: " + msd1);
+            FullPocket(data);
+        }
+        public static void ReadHoldingReg(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[2];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+            data[5] = 0;
+            data[6] = 2;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Read Holding Registers: " + msd1);
+            FullPocket(data);
+        }
+        public static void ReadInputReg(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[3];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+            data[5] = 0;
+            data[6] = 1;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Read Input Registers: " + msd1);
+            FullPocket(data);
+        }
+        public static void ForceSingleCoil(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[4];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+            data[5] = 0;
+            data[6] = 1;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Force Single Coil: " + msd1);
+            FullPocket(data);
+        }
+        public static void PresetSingleReg(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+            byte[] data = new byte[7];
+            data[0] = len[0];
+            data[1] = unit;
+            data[2] = func[5];
+            data[3] = startAddress[0];
+            data[4] = startAddress[1];
+            data[5] = 1;
+            data[6] = 2;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Preset Single Register: " + msd1);
+            FullPocket(data);
+        }
+        public static void ForceMultipleCoil(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+                byte[] data = new byte[9];
+                data[0] = len[2];
+                data[1] = unit;
+                data[2] = func[6];
+                data[3] = startAddress[0];
+                data[4] = startAddress[1];
+                data[5] = 0;
+                data[6] = 5;
+                data[7] = 1;
+                data[8] = 19;
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Force Multiple Coils: " + msd1);
+            FullPocket(data);
+        }
+        public static void PresetMultipleReg(byte[] len, byte unit, byte[] func, byte[] startAddress)
+        {
+                byte[] data = new byte[12];
+                data[0] = len[1];
+                data[1] = unit;
+                data[2] = func[7];
+                data[3] = startAddress[0];
+                data[4] = startAddress[1];
+                data[5] = 0;            // количество регистров
+                data[6] = 2;           // для чтения
+                data[7] = 4;            // количество байт далее
+                data[8] = 0;           // значение ст
+                data[9] = 10;           // значение мл
+                data[10] = 1;           // значение ст
+                data[11] = 2;           // значение мл
+            byte[] mess1 = Reg.func1.Concat(data).ToArray();
+            string msd1 = BitConverter.ToString(mess1);
+            Console.WriteLine("Запрос Preset Multiple Registers: " + msd1);
+            FullPocket(data);
+        }
+    }
+}
